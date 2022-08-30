49edc02ab1ef7d3060d27bd9a2d5f2c83b34fbba
Ot Vadim <v.otdelnov77@gmail.com>
2022-08-30 20:46:26 +0300

    games reducer

--- src/components/game-item/game-item.js
+++ src/components/game-item/game-item.js
@@ -1,12 +1,23 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import { GameCover } from '../game-cover';
 import { GameBuy } from '../game-buy';
 import { GameGenre } from '../game-genre';
 import './game-item.css';
+import { setCurrentGame } from "../../redux/games/reducer";
+
 
 export const GameItem = ({ game }) => {
+    const history = useHistory();
+    const dispatch = useDispatch();
+
+    const handleClick = () => {
+        dispatch(setCurrentGame(game));
+        history.push(`/app/${game.title}`)
+    };
     return (
-        <div className="game-item">
+        <div className="game-item" onClick={handleClick}>
             <GameCover image={ game.image }/>
             <div className="game-item__details">
                 <span className="game-item__title">{ game.title }</span>
